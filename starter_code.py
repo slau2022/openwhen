df0709fae7974a99f753d04bbaf656fda5aff6d6
Sophia Lau <slau2022@gmail.com>
2023-11-16 22:20:04 -0600

    Starter code

---
+++ starter_code.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import math
+
+def decode(input_file_path):
+    # your solution here!
+    img = Image.open(input_file_path)
+    
+input_file_path = "woodgrain.jpg"
+decode(input_file_path)
+
+    
